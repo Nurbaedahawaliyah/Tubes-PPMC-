cf45778916b998ffed090c3f4c26a392293a9cd2
Laura Anggelina Sha’ad <161745068+Lauraanggelina@users.noreply.github.com>
2024-05-17 01:57:20 +0700

    AntColonyOptimization.c

--- AntColonyOptimization.c
+++ AntColonyOptimization.c
@@ -5,7 +5,12 @@
 #include <time.h>
 
 #define MAX_LEN_STRING 255
-#define M_PI 3.14159265358979323846
+#define MAX_CITIES 100
+#define NUM_ANTS 10
+#define ALPHA 1.0 // Pengaruh pheromone
+#define BETA 2.0  // Pengaruh visibilitas (jarak)
+#define RHO 0.1   // Tingkat penguapan pheromone
+#define Q 100.0   // Jumlah pheromone yang dilepaskan oleh semut
 
 // Definisi struct Node untuk menyimpan data kota
 typedef struct Node {
@@ -14,6 +19,13 @@ typedef struct Node {
     double bujur;
     struct Node* next;
 } Node;
+
+// Definisi struct Ant untuk menyimpan status semut
+typedef struct Ant {
+    int tour[MAX_CITIES]; // Tur semut
+    int visited[MAX_CITIES]; // Status kunjungan
+    double tour_length; // Panjang tur
+} Ant;
 
 // Fungsi untuk menambahkan node baru ke linked list
 int add(Node **head, double data_lintang, double data_bujur, char nama[]) {
@@ -131,42 +143,105 @@ int find_city_index(Node* cities[], int numCities, char* cityName) {
     return -1;
 }
 
-// Fungsi rekursif DFS untuk menemukan jalur terdekat dalam masalah TSP
-void search_best_route(Node* cities[], int numCities, int visited[], int currPos, int count, double cost, double* minCost, double distances[numCities][numCities], int path[], int bestPath[], int startIndex) {
-    if (count == numCities) {
-        double returnCost = distances[currPos][startIndex];
-        if (returnCost > 0 && (cost + returnCost < *minCost)) {
-            *minCost = cost + returnCost;
-            for (int i = 0; i < numCities; i++) {
-                bestPath[i] = path[i];
-            }
-            bestPath[numCities] = startIndex; // Kembali ke kota awal
-        }
-        return;
-    }
-
-    for (int i = 0; i < numCities; i++) {
-        if (!visited[i] && distances[currPos][i] > 0) {
-            visited[i] = 1;
-            path[count] = i;
-            search_best_route(cities, numCities, visited, i, count + 1, cost + distances[currPos][i], minCost, distances, path, bestPath, startIndex);
-            visited[i] = 0;
-        }
+// Fungsi untuk inisialisasi semut
+void init_ants(Ant ants[], int numCities) {
+    for (int i = 0; i < NUM_ANTS; i++) {
+        memset(ants[i].visited, 0, sizeof(ants[i].visited));
+        ants[i].tour_length = 0.0;
     }
 }
 
-// Fungsi untuk mencetak rute terbaik
-void print_bestRoute(Node *cities[], int bestPath[], int jumlah_kota, double minCost) {
+// Fungsi untuk memilih kota berikutnya berdasarkan aturan ACO
+int select_next_city(Ant *ant, int numCities, double pheromones[][MAX_CITIES], double distances[][MAX_CITIES], double alpha, double beta) {
+    double total = 0.0;
+    double probabilities[numCities];
+
+    // Hitung total probabilitas
+    for (int i = 0; i < numCities; i++) {
+        if (ant->visited[i] == 0) {
+            total += pow(pheromones[ant->tour[numCities - 1]][i], alpha) * pow(1.0 / distances[ant->tour[numCities - 1]][i], beta);
+        }
+    }
+
+    // Hitung probabilitas untuk setiap kota yang belum dikunjungi
+    for (int i = 0; i < numCities; i++) {
+        if (ant->visited[i] == 0) {
+            probabilities[i] = (pow(pheromones[ant->tour[numCities - 1]][i], alpha) * pow(1.0 / distances[ant->tour[numCities - 1]][i], beta)) / total;
+        } else {
+            probabilities[i] = 0.0;
+        }
+    }
+
+    // Pilih kota berikutnya berdasarkan probabilitas
+    double r = (double)rand() / RAND_MAX;
+    double sum = 0.0;
+    for (int i = 0; i < numCities; i++) {
+        sum += probabilities[i];
+        if (sum >= r) {
+            return i;
+        }
+    }
+    // Jika gagal memilih, kota terakhir yang dipilih
+    for (int i = 0; i < numCities; i++) {
+        if (ant->visited[i] == 0) {
+            return i;
+        }
+    }
+    return -1; // Jika semua kota telah dikunjungi
+}
+
+// Fungsi untuk menemukan rute terpendek dengan menggunakan algoritma ACO
+void find_shortest_path(Node *cities[], int numCities, double distances[][MAX_CITIES], double pheromones[][MAX_CITIES]) {
+    srand(time(NULL)); // Inisialisasi seed untuk random number generator
+
+    Ant ants[NUM_ANTS];
+    double best_tour_length = INFINITY;
+    int best_tour[MAX_CITIES];
+
+    for (int iteration = 0; iteration < 1000; iteration++) {
+        init_ants(ants, numCities);
+
+        // Setiap semut memilih kota secara berurutan
+        for (int i = 0; i < numCities - 1; i++) {
+            for (int j = 0; j < NUM_ANTS; j++) {
+                int next_city = select_next_city(&ants[j], numCities, pheromones, distances, ALPHA, BETA);
+                ants[j].visited[next_city] = 1;
+                ants[j].tour[i + 1] = next_city;
+                ants[j].tour_length += distances[ants[j].tour[i]][next_city];
+            }
+        }
+
+        // Update pheromones
+        for (int i = 0; i < NUM_ANTS; i++) {
+            ants[i].tour_length += distances[ants[i].tour[numCities - 1]][ants[i].tour[0]]; // Kembali ke kota awal
+            if (ants[i].tour_length < best_tour_length) {
+                best_tour_length = ants[i].tour_length;
+                memcpy(best_tour, ants[i].tour, sizeof(best_tour));
+            }
+            for (int j = 0; j < numCities - 1; j++) {
+                pheromones[ants[i].tour[j]][ants[i].tour[j + 1]] += Q / ants[i].tour_length;
+                pheromones[ants[i].tour[j + 1]][ants[i].tour[j]] += Q / ants[i].tour_length;
+            }
+        }
+
+        // Penguapan pheromone
+        for (int i = 0; i < numCities; i++) {
+            for (int j = 0; j < numCities; j++) {
+                pheromones[i][j] *= (1.0 - RHO);
+            }
+        }
+    }
+
+    // Print best route
     printf("Best route found:\n");
-    for (int i = 0; i <= jumlah_kota; i++) {
-        printf("%s", cities[bestPath[i]]->nama_kota);
-        if (i < jumlah_kota) {
+    for (int i = 0; i < numCities; i++) {
+        printf("%s", cities[best_tour[i]]->nama_kota);
+        if (i < numCities - 1) {
             printf(" -> ");
         }
     }
-    printf("\nBest route distance: %lf km\n", minCost);
+    printf("\nBest route distance: %lf km\n", best_tour_length);
 }
-
 
 // Fungsi utama
 int main(void) {
@@ -177,53 +252,26 @@ int main(void) {
     }
 
     // Menghitung jumlah kota
-    int jumlah_kota = calculate_cities(daftar_kota);
+    int numCities = calculate_cities(daftar_kota);
     
     // Membuat array node kota
-    Node *cities[jumlah_kota];
-    make_cities_arrOfNode(daftar_kota, cities, jumlah_kota);
+    Node *cities[numCities];
+    make_cities_arrOfNode(daftar_kota, cities, numCities);
 
     // Membuat matriks jarak antar kota
-    double distances[jumlah_kota][jumlah_kota];
-    make_distanceMatrices(cities, jumlah_kota, distances);
+    double distances[MAX_CITIES][MAX_CITIES];
+    make_distanceMatrices(cities, numCities, distances);
 
-
-    char startingCity[MAX_LEN_STRING];
-    printf("Enter starting point: ");
-    scanf("%s", startingCity);
-
-    // Cari index dari kota yang akan dicari
-    int startIndex = find_city_index(cities, jumlah_kota, startingCity);
-
-    // Handling case jika kota start tidak ada
-    if (startIndex == -1) {
-        printf("Starting city not found\n");
-        return 0;
+    // Matriks pheromones untuk ACO
+    double pheromones[MAX_CITIES][MAX_CITIES];
+    for (int i = 0; i < numCities; i++) {
+        for (int j = 0; j < numCities; j++) {
+            pheromones[i][j] = 0.01; // Inisialisasi dengan nilai kecil
+        }
     }
 
-    // Jika ada, maka jalankan algoritma dfs dan hitung waktu eksekusi
-
-    // Inisialisasi array visited
-    int visited[jumlah_kota];
-    memset(visited, 0, sizeof(visited));
-    visited[startIndex] = 1; // Mulai dari kota yang dipilih
-    int path[jumlah_kota + 1];  // Untuk menyimpan jalur saat ini
-    int bestPath[jumlah_kota + 1];  // Untuk menyimpan jalur terbaik
-    path[0] = startIndex;
-    // Mulai DFS untuk mencari biaya minimum
-    double minCost = INFINITY;
-    clock_t start_time = clock();
-    search_best_route(cities, jumlah_kota, visited, startIndex, 1, 0, &minCost, distances, path, bestPath, startIndex);
-    clock_t end_time = clock();
-
-    // Hitung waktu eksekusi
-    double time_spent = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-    // Cetak rute terbaik
-    print_bestRoute(cities, bestPath, jumlah_kota, minCost);
-
-    // Cetak waktu hasil eksekusi
-    printf("Time elapsed: %.10f s\n", time_spent);
+    // Temukan rute terpendek dengan ACO
+    find_shortest_path(cities, numCities, distances, pheromones);
 
     return 0;
 }
